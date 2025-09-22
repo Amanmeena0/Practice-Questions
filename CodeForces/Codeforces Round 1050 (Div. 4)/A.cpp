37b06821c0d3d3bdf9efececa897f772f04e3c08
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2025-09-22 23:59:30 +0530

    update

---
+++ CodeForces/Codeforces Round 1050 (Div. 4)/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, n;
+        cin >> x >> n;
+        int result;
+
+        if (n % 2 == 0)
+        {
+            result = 0;
+        }
+        else
+        {
+            result = x;
+        }
+
+        cout << result << endl;
+    }
+    return 0;
+}
