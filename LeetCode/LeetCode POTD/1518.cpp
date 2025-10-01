362dcd32885e8f1b9d4795c50e81144a8f8a7226
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2025-10-01 13:15:09 +0530

    update

---
+++ LeetCode/LeetCode POTD/1518.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        
+    }
+};
+
+int main() {
+   Solution sol;
+   int result = sol;
+   cout<< result << endl;
+   return 0;
+}
