0db6e86808f350da54e4e6d3b83f3bf42474d83c
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2025-10-02 21:10:55 +0530

    update

--- OA Practice/2.cpp
+++ OA Practice/2.cpp
@@ -1,14 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int operationsBinaryString(char* str){
-    for(char c : str){
-        
+int OperationsBinaryString(char *str)
+{
+    if (str == NULL)
+        return -1;
+
+    int len = strlen(str);
+    
+    if (len == 0)
+        return -1;
+
+    int result = (str[0] - '0');
+
+    for (int i = 1; i < len; i += 2)
+    {
+        if (str[i] == 'A')
+        {
+            result = result & (str[i + 1] - '0');
+        }
+        else if (str[i] == 'B')
+        {
+            result = result | (str[i + 1] - '0');
+        }
+        else if (str[i] == 'C')
+        {
+            result = result ^ (str[i + 1] - '0');
+        }
+        else
+        {
+            return -1;
+        }
     }
+    return result;
 }
 
 int main()
 {
-
-    return 0;
+    string s;
+    getline(cin, s);
+    char *str = &s[0];
+    cout << OperationsBinaryString(str);
 }
