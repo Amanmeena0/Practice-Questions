c6e82f9a3c916d71a8cad68d6c5a339571b20405
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2025-10-03 16:02:10 +0530

    update

--- OA Practice/7.cpp
+++ OA Practice/7.cpp
@@ -1,7 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int productSmallestPair(int *array, int n, int sum)
+{
+    if (n < 2)
+        return -1;
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(array[i] > array[j]){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    int answer = array[0] + array[1];
+    int final = 0;
+    if(answer <= sum){
+        final = array[0] * array[1];
+    }else {
+        final = 0;
+    }
+    return final;
+}
 
-int main() {
+int main()
+{
+    int sum;
+    cin>>sum;
+    int n;
+    cin>>n;
+    int *array = new int[n];
+    for(int i = 0; i < n ; i++){
+        cin>>array[i];
+    }
+    cout<<productSmallestPair(array,n,sum);
     return 0;
 }
