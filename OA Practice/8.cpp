d209a2ee70394bbc8153d6a2a0c26b6869ecdc55
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2025-10-04 12:59:42 +0530

    update

---
+++ OA Practice/8.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+    int n;
+    cin>>n;
+    int array[];
+    for(int i = 0; i < n; i++){
+        cin>>array[i];
+    }
+
+    if(n == 1 || n == 0) cout<<"NOT FOUND";
+    int mid = n/2;
+    int left = 1;
+    int right = n;
+
+    sort(array.begin(),array.end());
+    while(left < right){
+
+
+    }
+
+
+    return 0;
+}
