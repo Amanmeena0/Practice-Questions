ffa8d8de1469b363683241aac50bf9580071b5b4
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2025-10-08 09:10:51 +0530

    update

--- LeetCode/Blind 75/19.cpp
+++ LeetCode/Blind 75/19.cpp
@@ -15,6 +15,35 @@ class Solution
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n)
     {
+
+        if (head == nullptr)
+            return head;
+        if (head->next == nullptr)
+            return head;
+
+        ListNode *first = head;
+        ListNode *second = head;
         
+        int count = 0;
+
+        while (first != nullptr)
+        {
+            first = first->next;
+            count++;
+        }
+
+        int oneNodeBeforeN = count - n - 1;
+
+        for (int i = 0; i < oneNodeBeforeN; i++)
+        {
+            second = second->next;
+        }
+
+        ListNode *temp = second->next;
+
+        temp->next = nullptr;
+        second->next = temp->next;
+        delete temp;
+        return head;
     }
 };
