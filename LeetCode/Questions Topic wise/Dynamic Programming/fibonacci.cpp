a2218d70777d822eba463e9ba4115b77af2f5d11
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2025-10-08 23:12:03 +0530

    update

---
+++ LeetCode/Questions Topic wise/Dynamic Programming/fibonacci.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// recursion
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+// memoisation
+int fibMemo(int n)
+{
+    if (n <= 1)
+        return n;
+    vector<int> dp(n + 1, -1);
+    if (dp[n] != -1)
+        return dp[n];
+    return dp[n] = fib(n - 1) + fib(n - 2);
+}
+
+// tabulations
+int fibTabu(int n)
+{
+    vector<int> dp(n + 1, -1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
+//space optimization
+int fibSpace(int n)
+{
+    int prev1 = 0;
+    int prev2 = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    return prev2;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int result = fibMemo(n);
+    int result1 = fib(n);
+    int result2 = fibTabu(n);
+    int result3 = fibSpace(n);
+    cout << result << " and " << result1 << " and " << result2 << " and " << result3 << endl;
+    return 0;
+}
