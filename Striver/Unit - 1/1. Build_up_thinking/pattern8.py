6588edb0fddcee87c199d73a40ca2a958e14181a
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-01-17 18:12:46 +0530

    update

--- Striver/Unit - 1/1. Build_up_thinking/pattern8.py
+++ Striver/Unit - 1/1. Build_up_thinking/pattern8.py
@@ -1,15 +1,14 @@
 class Solution:
     def pattern1(self, n):
-        for i in range(n):
-            for j in range(n - i - 1):
+        for i in range(n,0,-1):
+            for j in range(n - i  , 0, -1):
                 print(" ",end=" ")
-            
-            for j in range(2 * i  + 1):
+            for j in range(2 * i - 1, 0 , -1):
                 print("*",end=" ")
-            
-            for j in range(n - i - 1):
+            for j in range(n - i , 0, -1):
                 print(" ",end=" ")
             print()
+
 
 sol = Solution()
 N = 5
