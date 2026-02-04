241c0fe716ee18be0634076246c0f39907acb1f9
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-04 23:14:12 +0530

    update

--- Striver/Unit - 1/1. Build_up_thinking/pattern19.py
+++ Striver/Unit - 1/1. Build_up_thinking/pattern19.py
@@ -1,11 +1,21 @@
 class Solution:
     def pattern1(self, n):
         for i in range(n):
-            
+            for j in range(n - i ):
+                print("*",end="")
+            for j in range(2 * i + 1):
+                print(" ",end="")
+            for j in range(n-i):
+                print("*",end="")
 
             print()
-        for i in range(n):
-            
+        for i in range(n,0,-1):
+            for j in range(n - i+1 , 0, -1):
+                print("*",end="")
+            for j in range(2 * i - 1, 0 , -1):
+                print(" ",end="")
+            for j in range(n - i +1  , 0, -1):
+                print("*",end="")
             print()
 
 sol = Solution()
