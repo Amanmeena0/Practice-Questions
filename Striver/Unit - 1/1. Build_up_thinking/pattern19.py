f497fc1c250dd4bfa05fe097aace92ed6dd49dd2
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-04 22:39:36 +0530

    m

---
+++ Striver/Unit - 1/1. Build_up_thinking/pattern19.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pattern1(self, n):
+        for i in range(n):
+            
+
+            print()
+        for i in range(n):
+            
+            print()
+
+sol = Solution()
+N = 5
+sol.pattern1(N)
