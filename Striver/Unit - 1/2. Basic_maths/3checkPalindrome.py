b475bc588389b34276c826714eed2294a85cfac9
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-02-06 09:30:54 +0530

    meesage

--- Striver/Unit - 1/2. Basic_maths/3checkPalindrome.py
+++ Striver/Unit - 1/2. Basic_maths/3checkPalindrome.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # Negative numbers are not palindromes
         if x < 0:
             return False
         
---
+++ Striver/Unit - 1/2. Basic_maths/4Gcd.py
@@ -0,0 +1,11 @@
+class Solution:
+    def GCD(self, n1, n2):
+        
+        print()
+
+
+sol = Solution()
+n1 = 4
+n2 = 6
+
+print(sol.GCD(n1,n2))
